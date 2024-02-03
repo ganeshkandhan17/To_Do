330a1a22f94ffef26227d866e99e6fb11889dde4
ganeshkandhan17 <ganeshkandhan@gmail.com>
2024-02-04 01:30:42 +0530

    Small Changes

--- script.js
+++ script.js
@@ -1,9 +1,19 @@
-function addtask(){
+function addtask() {
     let input = document.querySelector("#input").value;
-    if(input == ""){
+    let des = document.querySelector("#des").value;
+    let date = document.querySelector('#date').value
+    date = date.split("T");
+    let time = date[1];
+    let timehs = time.split(":");
+    console.log(timehs);
+    let hr = timehs[0];
+    console.log(hr);
+    hr = parseInt(hr);
+    console.log(hr / 2);
+    if (input == "") {
         alert("Enter Task");
     }
-    else{
+    else {
         let divnode = document.createElement("div");
         divnode.className = "tasksli";
         let img = document.createElement("img");
@@ -19,83 +29,92 @@ function addtask(){
         divnode.appendChild(span);
         document.querySelector(".tasks").prepend(divnode);
         document.querySelector("#input").value = "";
+        document.querySelector("#des").value = "";
+        document.querySelector("#date").value = "";
+        let detail = document.createElement("div");
+        detail.className = "dtldiv";
+        let dp = document.createElement("p");
+        dp.className = "dtlp1";
+        let dd = document.createElement("p");
+        dd.className = "dtlp2"
+        dp.innerHTML = `<b>Description :</b> ${des}`;
+        dd.innerHTML = `<b>Expiration Date :</b> ${date}`;
+        console.log(detail);
+        detail.appendChild(dp);
+        detail.appendChild(dd);
+        divnode.appendChild(detail);
     }
 }
 let tasks = document.getElementById("tasks");
-tasks.addEventListener("click",function(e){
-    if(e.target.tagName=="DIV"){
-        let node=e.target.childNodes;
-        if(node[0].src.includes("img/checked.png")){
-            node[0].src="img/unchecked.png";
-            node[1].style.textDecoration="";
+tasks.addEventListener("click", function (e) {
+    if (e.target.tagName == "DIV") {
+        let node = e.target.childNodes;
+        if (node[0].src.includes("img/checked.png")) {
+            node[0].src = "img/unchecked.png";
+            node[1].style.textDecoration = "";
         }
-        else if(node[0].src.includes("img/unchecked.png")){
-            node[0].src="img/checked.png";
-            node[1].style.textDecoration="line-through";
+        else if (node[0].src.includes("img/unchecked.png")) {
+            node[0].src = "img/checked.png";
+            node[1].style.textDecoration = "line-through";
         }
     }
-    else if(e.target.tagName=="SPAN"){
+    else if (e.target.tagName == "SPAN") {
         e.target.parentElement.classList.add("active");
-        setTimeout(function(){
+        setTimeout(function () {
             e.target.parentElement.remove();
-        },250);
+        }, 250);
     }
-    else if(e.target.tagName=="IMG"){
-        let btn=e.target;
-        let tt=e.target.nextSibling;
-        if(btn.src.includes("img/checked.png")){
-            btn.src="img/unchecked.png"; 
-            tt.style.textDecoration="";
+    else if (e.target.tagName == "IMG") {
+        let btn = e.target;
+        let tt = e.target.nextSibling;
+        if (btn.src.includes("img/checked.png")) {
+            btn.src = "img/unchecked.png";
+            tt.style.textDecoration = "";
         }
-        else if(btn.src.includes("img/unchecked.png")){
-            btn.src="img/checked.png";
-            tt.style.textDecoration="line-through";
-        }  
+        else if (btn.src.includes("img/unchecked.png")) {
+            btn.src = "img/checked.png";
+            tt.style.textDecoration = "line-through";
+        }
     }
-    else if(e.target.tagName=="P"){
-        let btn=e.target.previousSibling;
-        let tt=e.target;
-        if(btn.src.includes("img/checked.png")){
-            btn.src="img/unchecked.png"; 
-            tt.style.textDecoration="";
+    else if (e.target.tagName == "P") {
+        let btn = e.target.previousSibling;
+        let tt = e.target;
+        if (btn.src.includes("img/checked.png")) {
+            btn.src = "img/unchecked.png";
+            tt.style.textDecoration = "";
         }
-        else if(btn.src.includes("img/unchecked.png")){
-            btn.src="img/checked.png";
-            tt.style.textDecoration="line-through";
+        else if (btn.src.includes("img/unchecked.png")) {
+            btn.src = "img/checked.png";
+            tt.style.textDecoration = "line-through";
         }
-    }  
+    }
 });
-
-// let span=document.querySelector(".close");
-// span.addEventListener("click",function(){
-//     let layover=document.querySelector(".layover");
-//         layover.classList.add("hide");
-//         setTimeout(function(){
-//             layover.remove();
-//         },600);
-// });
-
-document.getElementById("input").addEventListener("keydown",function(e){
-    if(e.code=="Enter"){
+document.getElementById("input").addEventListener("keydown", function (e) {
+    if (e.code == "Enter") {
         addtask();
     }
 });
-document.querySelector(".seaimg").addEventListener('click',()=>{
-    let seain=document.querySelector('.seain');
+document.querySelector(".seaimg").addEventListener('click', () => {
+    let seain = document.querySelector('.seain');
     seain.classList.toggle('active');
 });
-let logout=document.querySelector(".logout");
-logout.addEventListener('click',()=>{
+let logout = document.querySelector(".logout");
+logout.addEventListener('click', () => {
     document.querySelector('.prodrp').classList.toggle('active');
 })
 
-let taskin=document.querySelector('.taskinput');
-taskin.addEventListener('focus',(e)=>{
+let taskin = document.querySelector('.taskinput');
+taskin.addEventListener('focus', (e) => {
+    console.log("1")
     document.querySelector('#des').removeAttribute("hidden");
     document.querySelector('#date').removeAttribute("hidden");
+    console.log("2")
 })
 
 taskin.addEventListener('blur', (e) => {
-    document.querySelector('#des').setAttribute("hidden", "");
-    document.querySelector('#date').setAttribute("hidden", "");
+    console.log("3")
+    if (taskin.value == "") {
+        document.querySelector('#des').setAttribute("hidden", "");
+        document.querySelector('#date').setAttribute("hidden", "");
+    }
 });
